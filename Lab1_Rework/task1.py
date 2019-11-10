10addf815e08f4b3434b0fd0bd260fea2cacfe5b
igortereshchenko <tereshchenko.igor@gmail.com>
2019-11-10 19:41:52 +0200

    Lab_1 print repair

--- Lab1_Rework/task1.py
+++ Lab1_Rework/task1.py
@@ -4,4 +4,4 @@ def execute():
     print("Введіть кількість градусів Цельсія:")
     tempCelsius = func.float_Validator()
     tempFahrenheit = (tempCelsius * (9 / 5)) + 32
-    print(tempCelsius, "градусів Цельсія = ", tempFahrenheit, "градусів Фаренгейта")
+    print("{1} градусів Цельсія = {2} градусів Фаренгейта".format(tempCelsius, tempFahrenheit))
