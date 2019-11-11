6cbddfe00792ec56448dc6c4d7dd07bbba63478f
Illia Maistruk <illiuaspire@gmail.com>
2019-11-12 00:20:39 +0200

    rework

--- Lab3/func.py
+++ Lab3/func.py
@@ -1,28 +1,51 @@
-def checkType(a):
-    i = 1
-    if a == "int":
-        while i == 1:
-            try:
-                v = int(input())
-                i += 1
-            except:
-                print("Потрібно ввести число")
-        return v
-    elif a == "float":
-        while i == 1:
-            try:
-                v = float(input())
-                i += 1
-            except:
-                print("Потрібно ввести число")
-        return v
+import re
+
+def is_one_symbol(start_str):
+    return bool(re.match(r"^\S$", start_str))
+
+def is_integer(start_str):
+    return bool(re.match(r"^[+-]?\d+$", start_str))
+
+def is_negative_integer(start_str):
+    return bool(re.match(r"^[-]?\d+$", start_str))
+
+def is_positive_integer(start_str):
+    return bool(re.match(r"^[+]?\d+$", start_str))
+
+def is_float(start_str):
+    return bool(re.match(r"^[+-]?\d+\.?\d*$", start_str))
+
+def int_Validator(prop_str="", type_of=""):
+    temp_int = input(prop_str)
+    if type_of == "+":
+        while not is_positive_integer(temp_int):
+            temp_int = input("Введіть ціле додатнє число: ")
+    elif type_of == "-":
+        while not is_negative_integer(temp_int):
+            temp_int = input("Введіть ціле від'ємне число: ")
+    else:
+        while not is_integer(temp_int):
+            temp_int = input("Введіть ціле число: ")
+    return int(temp_int)
+
+def one_symbol(prop_str=""):
+    temp_symbol = input(prop_str)
+    while not is_one_symbol(temp_symbol):
+        temp_symbol = input("Введіть один символ: ")
+    return temp_symbol
+
+def float_Validator(prop_str=""):
+    temp_float = input(prop_str)
+    while not is_float(temp_float):
+        temp_float = input(prop_str)
+    return float(temp_float)
 
 def setChoice():
     print("Ви би хотіли розпочати знову?\n1) так\n2) ні")
     i = 1
     j = 1
     while i == 1:
-        c = checkType("int")
+        c = int_Validator()
         if c == 1:
             print("Починаэмо спочатку")
             i += 1
